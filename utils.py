4f91b04a8fe9a683717708192f01956ecaa8792e
Simon-Bae <simon.bae825@gmail.com>
2025-02-21 20:51:30 +0900

    feat: Add load california housing data method
    - utils.py 내부 sklearn의 california housing 데이터를 로드하는 메서드를 추가

--- utils.py
+++ utils.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 import torch
 
+from sklearn.datasets import fetch_california_housing
 from sklearn.preprocessing import StandardScaler
 
 def load_boston_house_prices_data():
@@ -20,3 +21,23 @@ def load_boston_house_prices_data():
 
     return x, y
 
+def load_california_housing_data():
+    california = fetch_california_housing()
+
+    df = pd.DataFrame(california.data, columns=california.feature_names)
+    df.tail()
+
+    # Standardization - mean = 0, variance = 1
+    scaler = StandardScaler()
+    scaler.fit(df.values[:, :-1])
+    df.values[:, :-1] = scaler.transform(df.values[:, :-1])
+
+    # Add target column
+    df["Target"] = california.target
+
+    # Dataframe to Tensor
+    data = torch.from_numpy(df.values).float()
+
+    x, y = data[:, :-1], data[:, -1:]
+
+    return x, y
