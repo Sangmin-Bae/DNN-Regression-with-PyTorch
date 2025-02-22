9a67e091156907e07db6c92e90920a2f24b5b5e3
Simon-Bae <simon.bae825@gmail.com>
2025-02-22 18:35:19 +0900

    refactor: modify train.py
    - config 인수 호출 방식 변경 config.argument > config['key']
    - 단일화된 데이터 로드 메서드와 DNN 모델 적용

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
-from model import MyDNNModelV2
+from model import MyDNNModel
 from trainer import Trainer
 
 from utils import load_data
@@ -26,17 +26,17 @@ def load_config(path):
 
 def main(config):
     # Set device
-    device = torch.device("cpu") if config.gpu_id < 0  else torch.device(f"cuda:{config.gpu_id}")
+    device = torch.device("cpu") if config.gpu_id < 0  else torch.device(f"cuda:{config['gpu_id']}")
     print(f"Device : {device}")
 
     # Load Data
-    x, y = load_data(config.data_number)
+    x, y = load_data()
 
     print(f"Train Data : {x.shape}")
     print(f"Target Data : {y.shape}")
 
     # Define model
-    model = MyDNNModelV2(input_size=x.size(-1), output_size=y.size(-1)).to(device)
+    model = MyDNNModel(input_size=x.size(-1), output_size=y.size(-1)).to(device)
     print(f"Model: {model}")
 
     # Set optimizer
@@ -52,7 +52,7 @@ def main(config):
         "model": trainer.model.state_dict(),
         "opt": optimizer.state_dict(),
         "config": config
-    }, config.model_fn)
+    }, config["model_fn"])
 
 if __name__ == "__main__":
     args = argument_parser()
