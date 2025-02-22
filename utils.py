91ec7c5bd561d0024d91ea55a6b72bcde2ea53d6
Simon-Bae <simon.bae825@gmail.com>
2025-02-22 18:00:40 +0900

    refactor: modify utils.py
    - 실험 데이터 단일화 작업(california housing 데이터)
    - boston house prices 데이터 로드 메서드 제거
    - 선택적 데이터 로드 메서드 제거

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-import numpy as np
 import pandas as pd
 
 import torch
@@ -6,22 +5,7 @@ import torch
 from sklearn.datasets import fetch_california_housing
 from sklearn.preprocessing import StandardScaler
 
-def load_boston_house_prices_data():
-    df = pd.read_csv("./data/boston_house_prices.csv", skiprows=1, dtype=np.float32)
-    df.rename(columns={"MEDV" : "TARGET"}, inplace=True)
-
-    # Standardization - mean = 0, variance = 1
-    scaler = StandardScaler()
-    scaler.fit(df.values[:, :-1])
-    df.values[:, :-1] = scaler.transform(df.values[:, :-1])
-
-    data = torch.from_numpy(df.values).float()
-
-    x, y = data[:, :-1], data[:, -1:]
-
-    return x, y
-
-def load_california_housing_data():
+def load_data():
     california = fetch_california_housing()
 
     df = pd.DataFrame(california.data, columns=california.feature_names)
@@ -39,15 +23,5 @@ def load_california_housing_data():
     data = torch.from_numpy(df.values).float()
 
     x, y = data[:, :-1], data[:, -1:]
-
-    return x, y
-
-def load_data(data_number):
-    if data_number == 1:
-        x, y = load_boston_house_prices_data()
-    elif data_number == 2:
-        x, y = load_california_housing_data()
-    else:
-        raise ValueError()
 
     return x, y
